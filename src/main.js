9b667f6194fd4279ba65b6348ef42f2798cd8436
DmitriLyalin <donlyalin@gmail.com>
2026-09-09 14:16:45 +0300

    add uniquId, parse rss funtcion, id generator

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { proxy, subscribe, snapshot } from 'valtio/vanilla'
 import { renderState } from './view.js'
 import i18nextInstance from './i18next.js'
 import axios from 'axios'
+import { uniqueId } from 'es-toolkit/compat'
 const schema = yup.string()
   .trim()
   .required('emptyUrl')
@@ -25,25 +26,28 @@ const schema = yup.string()
 //         return response.data.status.http_code === 200})
 //       .catch(() => false)
 //   })
- 
+
+
 const loadData = (url) => {
   return axios.get('https://allorigins.hexlet.app/get', {
-    params: {disableCache: true, url}
+    params: { disableCache: true, url }
   }).then((response) => {
     return response.data.contents
   })
-  .catch((err) => {
-    err.message = 'networkError'
-    return Promise.reject(err)} )
+    .catch((err) => {
+      err.message = 'networkError'
+      return Promise.reject(err)
+    })
 }
 const state = proxy({
-  ui : {
+  ui: {
     status: 'filling',
   },
-  data : {
+  data: {
     error: null,
     successMsg: null,
     feed: [],
+    posts: [],
   }
 })
 const validateUrl = (url) => {
@@ -61,43 +65,59 @@ const form = document.querySelector('form')
 const parseRss = (rss) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(rss, "application/xml");
+  console.log(doc)
   if (doc.querySelector('parsererror')) {
     throw new Error('invalidRss')
   }
   else {
-    console.log('doc', doc)
-    return doc
+      const channelTitle = doc.querySelector('channel > title').textContent
+      const channelDescription = doc.querySelector('channel > description').textContent
+      const id = uniqueId()
+      const posts = doc.querySelectorAll('item')
+      const links = [...posts].map((post) => post.querySelector('link').textContent)
+      
+      return {id,channelTitle, channelDescription, links}
   }
 
-  
 }
+const createIdGenerator = (start = 1) => {
+  let count = start;
+  return () => count++;
+};
+const counter =createIdGenerator()
+console.log(counter)
+console.log(counter)
 form.addEventListener('submit', (e) => {
-   state.data.error = null
-  e.preventDefault();
+  state.data.error = null
+  e.preventDefault(); 
   const formData = new FormData(e.target)
   const url = formData.get('url-input')
   validateUrl(url)
-  .then(url =>loadData(url))
-  .then((rss) => {
-    parseRss(rss)
-  })
-  .catch((err) => {
-   
-    state.data.error = keyFromSelector(($) => $.errors[err.message])
-  })
-  
+    .then(url => loadData(url))
+    .then((rss) => parseRss(rss))
+    .then((data) => {
+      state.data.feed.push({id: data.id, title:data.channelTitle, description:data.channelDescription, url: url})
+      data.links.forEach((link) => state.data.posts.push({title: link, id:counter(), feedId: data.id}))
+      console.log(state.data)
+    })
+    .catch((err) => {
+
+      state.data.error = keyFromSelector(($) => $.errors[err.message])
+    })
+
 
 })
 
 subscribe(state.data, () => {
-  
-    const watchedState = snapshot(state.data);
 
-    console.log(watchedState)
-    renderState(document.getElementById('app'), watchedState, inputUrl)
-    console.log('feed', watchedState.feed)
-    console.log('errors', watchedState.error)
-  })
+  const watchedState = snapshot(state.data);
+
+  console.log(watchedState)
+  renderState(document.getElementById('app'), watchedState, inputUrl)
+  console.log('feed', watchedState.feed)
+  console.log('posts', watchedState.posts)
+  console.log('errors', watchedState.error)
+})
 
 // renderState(document.getElementById('app'), state, inputUrl)
-export {state}
+export { state }
