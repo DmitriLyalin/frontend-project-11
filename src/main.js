4a635e33b49f0f489aefd05e4e946bf869b2bdb7
DmitriLyalin <donlyalin@gmail.com>
2026-09-15 10:32:57 +0300

    refactor: updata RSS and UI  for new posts

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { proxy, subscribe, snapshot } from 'valtio/vanilla'
 import { renderState } from './view.js'
 import i18nextInstance from './i18next.js'
 import axios from 'axios'
-import { uniqueId } from 'es-toolkit/compat'
+// import { uniqueId } from 'es-toolkit/compat'
 const schema = yup.string()
   .trim()
   .required('emptyUrl')
@@ -70,13 +70,13 @@ const parseRss = (rss) => {
     throw new Error('invalidRss')
   }
   else {
-      const channelTitle = doc.querySelector('channel > title').textContent
-      const channelDescription = doc.querySelector('channel > description').textContent
-      const id = uniqueId()
-      const posts = doc.querySelectorAll('item')
-      const links = [...posts].map((post) => post.querySelector('link').textContent)
-      
-      return {id,channelTitle, channelDescription, links}
+
+    const channelTitle = doc.querySelector('channel > title').textContent
+    const channelDescription = doc.querySelector('channel > description').textContent
+    const posts = doc.querySelectorAll('item')
+    const links = [...posts].map((post) => post.querySelector('link').textContent)
+
+    return { channelTitle, channelDescription, links }
   }
 
 }
@@ -84,23 +84,26 @@ const createIdGenerator = (start = 1) => {
   let count = start;
   return () => count++;
 };
-const counter =createIdGenerator()
-console.log(counter)
-console.log(counter)
+
+const updateUi = (data, url) => {
+  const feedId = feedCounter()
+  const postId = postCounter()
+  state.data.feed.push({ id: feedId, title: data.channelTitle, description: data.channelDescription, url: url })
+  data.links.forEach((link) => state.data.posts.push({ id: postId, title: link, feedId: feedId }))
+  state.data.error = null
+}
+const feedCounter = createIdGenerator()
+const postCounter = createIdGenerator()
 form.addEventListener('submit', (e) => {
-  
-  e.preventDefault(); 
+
+  e.preventDefault();
   const formData = new FormData(e.target)
   const url = formData.get('url-input')
   validateUrl(url)
     .then(url => loadData(url))
     .then((rss) => parseRss(rss))
-    .then((data) => {
-      state.data.feed.push({id: data.id, title:data.channelTitle, description:data.channelDescription, url: url})
-      data.links.forEach((link) => state.data.posts.push({title: link, id:counter(), feedId: data.id}))
-      console.log(state.data)
-      state.data.error = null
-    })
+    .then((data) => updateUi(data, url))
+    .then(() => setTimeout(checkForNewPosts()), 5000)
     .catch((err) => {
 
       state.data.error = keyFromSelector(($) => $.errors[err.message])
@@ -114,11 +117,58 @@ subscribe(state.data, () => {
   const watchedState = snapshot(state.data);
 
   console.log(watchedState)
-  renderState(document.getElementById('form-container'),document.getElementById('feed'),document.getElementById('posts'), watchedState, inputUrl)
-  console.log('feed', watchedState.feed)
-  console.log('posts', watchedState.posts)
-  console.log('errors', watchedState.error)
+  renderState(document.getElementById('form-container'), document.getElementById('feed'), document.getElementById('posts'), watchedState, inputUrl)
+  // console.log('feed', watchedState.feed)
+  // console.log('posts', watchedState.posts)
+  // console.log('errors', watchedState.error)
 })
+const checkForNewPosts = () => {
+  const watchedState = snapshot(state.data)
+  console.log('checkForNewPosts', watchedState)
+  const promises = watchedState.feed.map((item) => {
+    return loadData(item.url).then((data) => ({ url: item.url, data }))
+  })
 
-// renderState(document.getElementById('app'), state, inputUrl)
+  console.log('promises', promises)
+  const promise = Promise.allSettled(promises)
+  
+  promise.then((filtered) => {
+    const fulfilled = filtered.filter((item) => item.status === 'fulfilled').map((item) => ({ url: item.value.url, data: item.value.data }))
+    const rejected = filtered.filter((item) => item.status === 'rejected')
+    rejected.forEach((item) => {
+      state.data.error = keyFromSelector(($) => $.errors[item.reason.message]) 
+    })
+    return fulfilled
+  })
+    .then((fulfilled) => {
+      return fulfilled.map((rss) => {
+        return { url: rss.url, data: parseRss(rss.data) }
+      })
+    })
+    .then((parsed) => {
+      console.log('parsed', parsed)
+      parsed.forEach((data) => {
+        // const watchedState = snapshot(state.data)
+        console.log('watchedState', watchedState)
+        const existingFeed = watchedState.feed.find((item) => item.url === data.url)
+        const newPosts = watchedState.posts.filter((item) => item.feedId === existingFeed.id)
+        const newPostsTitles = new Set(newPosts.map(item => item.title));
+        const result = data.data.links.filter(link => !newPostsTitles.has(link))
+        console.log(result)
+        console.log(existingFeed)
+         result.forEach((link) => state.data.posts.push({ id: 5, title: link, feedId: existingFeed.id }))
+         setTimeout(checkForNewPosts, 5000)
+      })
+    })
+    .catch((err) => {
+
+      state.data.error = keyFromSelector(($) => $.errors[err.message])
+    })
+
+ 
+}
+
 export { state }
+
+
+
