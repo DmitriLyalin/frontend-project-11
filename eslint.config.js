6953b5ac96bc7c5d42127618bf077bab5f3eb683
DmitriLyalin <donlyalin@gmail.com>
2026-09-24 11:15:49 +0300

    refactor: remove Playwright-related global ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,5 @@ import globals from "globals";
 import { defineConfig, globalIgnores  } from "eslint/config";
 
 export default defineConfig([
-   globalIgnores([".github/*", "node_modules/*", "playwright-report/**", "playwright.config.js", "**/dist/**"]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+   globalIgnores([".github/*", "node_modules/*", "**/dist/**"]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
