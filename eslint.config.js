4b9c09a55f4ccd839e970049990767a926d88e90
DmitriLyalin <donlyalin@gmail.com>
2026-09-24 10:53:10 +0300

    refactor: update ESLint configuration to include additional global ignores

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,5 @@ import globals from "globals";
 import { defineConfig, globalIgnores  } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores([".github/*",]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+   globalIgnores([".github/*", "node_modules/*", "playwright-report/**", "playwright.config.js"]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores([".github/*", "node_modules/*", "playwright-report/**", "playwright.config.js"]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+   globalIgnores([".github/*",  "playwright-report/**", "playwright.config.js", "dist/**"]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
