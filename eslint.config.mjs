35a9d9b2256518413d5115d331cc7d8a5a6b5021
DmitriLyalin <donlyalin@gmail.com>
2026-09-24 10:41:15 +0300

    refactor: add global ignores to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  globalIgnores([".github/*", "node_modules/*", "playwright-report/**", "playwright.config.js"]), { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
