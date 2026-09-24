4ca4ed15d975585a6e714778ca6bdda15bd261fa
DmitriLyalin <donlyalin@gmail.com>
2026-09-24 15:14:54 +0300

    refactor: improve code readability and structure in main.js and view.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { keyFromSelector } from "i18next";
 import { proxy, subscribe, snapshot } from 'valtio/vanilla'
 import { renderPosts, renderFeed, renderMessage, renderModal } from './view.js'
 import axios from 'axios'
-// validation schema
+// схема валидации URL
 const schema = yup.string()
   .trim()
   .required('emptyUrl')
@@ -14,14 +14,14 @@ const schema = yup.string()
     const { feed } = watchedState
     return !feed.some((item) => item.url === value)
   })
- // создание timerID
+// создание timerID
 let timerID = null
 // генерация уникального ID
 const createIdGenerator = (start = 1) => {
   let count = start;
   return () => count++;
 };
- // обработчик клика на Посты для выявления активного поста
+// обработчик клика на Посты для выявления активного поста
 const postContainer = document.getElementById('posts')
 postContainer.addEventListener('click', (e) => {
   state.ui.activePost = null
@@ -36,7 +36,7 @@ postContainer.addEventListener('click', (e) => {
 const feedCounter = createIdGenerator()
 const postCounter = createIdGenerator()
 
-//Загрузка данных с целевого url
+//Загрузка данных с сервера
 
 const loadData = (url) => {
   return axios.get('https://allorigins.hexlet.app/get', {
@@ -50,18 +50,22 @@ const loadData = (url) => {
     })
 }
 
-//состояние объекта
+//создание состояния приложения
 const state = proxy({
   ui: {
-    activePost:null,
+    formError: {
+      error:null
+    },
+    activePost: null,
   },
   data: {
-    error: null,
+    // error: null,
     successMsg: null,
     feed: [],
     posts: [],
   }
 })
+// функция валидации URL
 const validateUrl = (url) => {
   return schema
     .validate(url)
@@ -70,8 +74,8 @@ const validateUrl = (url) => {
       return Promise.reject(err)
     })
 }
-const form = document.querySelector('form')
 
+// функция парсинга RSS
 const parseRss = (rss) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(rss, "application/xml");
@@ -79,14 +83,16 @@ const parseRss = (rss) => {
     throw new Error('invalidRss')
   }
   else {
+    // получение данных из RSS
     const channelTitle = doc.querySelector('channel > title').textContent
     const channelDescription = doc.querySelector('channel > description').textContent
     const posts = doc.querySelectorAll('item')
     const links = [...posts].map((post) => {
+      // получение данных из каждого поста
       const postTitle = post.querySelector('title').textContent
       const postLink = post.querySelector('link').textContent
       const postDescription = post.querySelector('description').textContent
-      return {postTitle, postLink, postDescription}
+      return { postTitle, postLink, postDescription }
     })
 
     return { channelTitle, channelDescription, links }
@@ -94,16 +100,17 @@ const parseRss = (rss) => {
 
 }
 
-
+// функция обновления состояния приложения
 const updateUi = (data, url) => {
-
-  state.data.feed.push({ id: feedCounter(), title: data.channelTitle, description: data.channelDescription, url: url })
-  data.links.forEach((link) => state.data.posts.push({ id: postCounter(), isSeen: false, title: link.postTitle, postUrl: link.postLink, postDescription: link.postDescription, feedId: state.data.feed[state.data.feed.length - 1].id }))
-  state.data.error = null
+  const newFeed = { id: feedCounter(), title: data.channelTitle, description: data.channelDescription, url: url }
+  state.data.feed.push(newFeed)
+  data.links.forEach((link) => state.data.posts.push({ id: postCounter(), isSeen: false, title: link.postTitle, postUrl: link.postLink, postDescription: link.postDescription, feedId:newFeed.id }))
+  state.ui.formError.error = null
 }
-
+// обработчик события отправки формы
+const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
-
+//предотвращение поведения по умолчанию
   e.preventDefault();
   const formData = new FormData(e.target)
   const url = formData.get('url-input')
@@ -112,6 +119,7 @@ form.addEventListener('submit', (e) => {
     .then((rss) => parseRss(rss))
     .then((data) => updateUi(data, url))
     .then(() => {
+      //проверка таймера функции по поиску новых постов
       if (timerID === null) {
         return checkForNewPosts()
       }
@@ -121,48 +129,54 @@ form.addEventListener('submit', (e) => {
     })
     .catch((err) => {
 
-      state.data.error = keyFromSelector(($) => $.errors[err.message])
+       state.ui.formError.error= keyFromSelector(($) => $.errors[err.message])
     })
-
-
 })
+//рендер модального окна с подробностями о посте
 subscribe(state.ui, () => {
-renderModal(document.querySelector('section'), state)
+  const watchedState = snapshot(state);
+  renderModal(document.querySelector('section'), watchedState)
 })
+//рендер постов
 subscribe(state.data.posts, () => {
   const watchedState = snapshot(state.data);
   renderPosts(postContainer, watchedState)
 })
+//рендер фидов
 subscribe(state.data.feed, () => {
   const watchedState = snapshot(state.data);
   renderFeed(document.getElementById('feed'), watchedState)
 })
+//рендер сообщения об ошибке
 subscribe(state.data, () => {
-  const watchedState = snapshot(state.data);
+  const watchedState = snapshot(state);
   renderMessage(document.getElementById('form-container'), watchedState)
 })
+//функция отслеживания новых постов в фидах
 const checkForNewPosts = () => {
 
   const watchedState = snapshot(state.data)
   const promises = watchedState.feed.map((item) => {
     return loadData(item.url).then((data) => ({ url: item.url, data }))
   })
-
+  //возврат промиса и фильтрация постов
   const promise = Promise.allSettled(promises)
-
   promise.then((filtered) => {
     const fulfilled = filtered.filter((item) => item.status === 'fulfilled').map((item) => ({ url: item.value.url, data: item.value.data }))
     const rejected = filtered.filter((item) => item.status === 'rejected')
+   //вывод сообщения об ошибке
     rejected.forEach((item) => {
-      state.data.error = keyFromSelector(($) => $.errors[item.reason.message])
+      state.ui.formError.error = keyFromSelector(($) => $.errors[item.reason.message])
     })
     return fulfilled
   })
+  // передача успешных постов в парсинг и проброс url
     .then((fulfilled) => {
       return fulfilled.map((rss) => {
         return { url: rss.url, data: parseRss(rss.data) }
       })
     })
+    // проверка текущего фида на наличие новых постов
     .then((parsed) => {
       parsed.forEach((data) => {
         const existingFeed = watchedState.feed.find((item) => item.url === data.url)
@@ -173,9 +187,10 @@ const checkForNewPosts = () => {
 
       })
     })
+    // отображение пойманной ошибки
     .catch((err) => {
 
-      state.data.error = keyFromSelector(($) => $.errors[err.message])
+      state.ui.formError.error = keyFromSelector(($) => $.errors[err.message])
     })
 
   timerID = setTimeout(checkForNewPosts, 5000)
--- src/view.js
+++ src/view.js
@@ -1,49 +1,55 @@
 import i18nextInstance from './i18next.js'
+// импортируем svg иконку закрытия модального окна
 import closeIconRaw from './assets/closeBtn.svg?raw'
-
+//функция рендера постов
 const renderPosts = (postsContainer, state) => {
   postsContainer.innerHTML = ''
   postsContainer.classList.add('border', 'border-gray-300', 'mb-4')
+  // создание заголовка и списка постов
   const postContainerTitle = document.createElement('h2')
   postContainerTitle.textContent = i18nextInstance.t(($) => $.ui.postsSection)
   postContainerTitle.classList.add('text-black', 'text-2xl', 'max-md:text-xl', 'font-bold', 'mb-2', 'px-2')
   const postList = document.createElement('ul')
+  // рендеринг постов
   state.posts.forEach((post) => {
-
     const postItem = document.createElement('li')
     postItem.classList.add('flex', 'justify-between', 'border-b-1', 'border-gray-300', 'items-center')
     const postLink = document.createElement('a')
-
-    const viewButton = document.createElement('button')
-    viewButton.classList.add('border', 'border-cyan-500', 'my-2', 'mx-4', 'px-2', 'py-1', 'rounded', 'text-cyan-500', 'cursor-pointer')
-    viewButton.textContent = i18nextInstance.t(($) => $.ui.buttons.view)
     postLink.setAttribute('href', `${post.postUrl}`)
+    // добавление атрибута data-seen и класса для поста
     postLink.dataset.seen = 'false'
     postLink.classList.add('font-bold', 'text-cyan-500', 'px-4')
+    // проверка, был ли пост просмотрен
     if (post.isSeen) {
       postLink.dataset.seen = 'true'
       postLink.classList.add('text-gray-300')
       postLink.classList.remove('font-bold')
     }
     postLink.textContent = post.title
+    // создание кнопки просмотра поста
+    const viewButton = document.createElement('button')
+    viewButton.classList.add('border', 'border-cyan-500', 'my-2', 'mx-4', 'px-2', 'py-1', 'rounded', 'text-cyan-500', 'cursor-pointer')
+    viewButton.textContent = i18nextInstance.t(($) => $.ui.buttons.view)
+
     postItem.append(postLink, viewButton)
 
     postList.appendChild(postItem)
-   
-    
+
   })
-  postsContainer.append(postContainerTitle,postList)
+  postsContainer.append(postContainerTitle, postList)
 }
+// функция рендера фидов
 const renderFeed = (feedContainer, state) => {
   feedContainer.innerHTML = ''
   feedContainer.classList.add('border', 'border-gray-300', 'mb-4')
- const feedContainerTitle = document.createElement('h2')
+  const feedContainerTitle = document.createElement('h2')
   feedContainerTitle.textContent = i18nextInstance.t(($) => $.ui.feedSection)
   feedContainerTitle.classList.add('text-black', 'text-2xl', 'max-md:text-xl', 'font-bold', 'mb-2', 'px-2')
   const feedList = document.createElement('ul')
+  // рендеринг фидов
   state.feed.forEach((item) => {
     const listItem = document.createElement('li')
-    listItem.classList.add('border-b-1', 'border-gray-300', 'text-sm', 'mb-2','py-2')
+    listItem.classList.add('border-b-1', 'border-gray-300', 'text-sm', 'mb-2', 'py-2')
     const feedTitle = document.createElement('h3')
     feedTitle.classList.add('text-black', 'font-bold', 'mb-1', 'px-2')
     feedTitle.textContent = item.title
@@ -54,82 +60,80 @@ const renderFeed = (feedContainer, state) => {
     feedList.appendChild(listItem)
   })
 
-
-
   feedContainer.append(feedContainerTitle, feedList)
 }
+// функция рендера сообщений об ошибках и успешных действиях
 const renderMessage = (messageContainer, state) => {
-  document.querySelector('input').classList.remove('ring-2', 'ring-red-500')
+  // удаление предыдущего сообщения и окрашивание инпута в красный цвет при ошибке
+  const inputElement = document.querySelector('input')
+  inputElement.classList.remove('ring-2', 'ring-red-500')
   const messageElement = document.createElement('div')
   messageElement.classList.add('text-sm', 'mt-2')
   messageElement.setAttribute('id', 'message')
+  // отображение сообщения об ошибке или успешном действии
   const { error } = state
   messageElement.textContent = error
     ? i18nextInstance.t(error)
     : i18nextInstance.t(($) => $.valid);
   if (error) {
-    console.log(error)
     messageElement.classList.add('text-red-500')
-    document.querySelector('input').classList.add('ring-2', 'ring-red-500')
+    inputElement.classList.add('ring-2', 'ring-red-500')
   }
 
   else {
-    
+    inputElement.value = ''
+    inputElement.focus()
     messageElement.classList.add('text-green-500')
   }
+  // удаление предыдущего сообщения и добавление нового
   messageContainer.querySelector('#message')?.remove()
   messageContainer.append(messageElement)
 }
 
-
+// функция рендера модального окна
 const renderModal = (container, state) => {
- container.querySelector('dialog')?.remove()
-console.log('renderModal', state)
+  // удаление предыдущего модального окна
+  container.querySelector('dialog')?.remove()
   const dialogWindow = document.createElement('dialog')
-  dialogWindow.classList.add('fixed', 'inset-0', 'm-auto',  'gap-2', 'border', 'py-4', 'border-black-500', 'rounded-lg')
+  dialogWindow.classList.add('fixed', 'inset-0', 'm-auto', 'gap-2', 'border', 'py-4', 'border-black-500', 'rounded-lg')
   dialogWindow.dataset.test = 'modal-body'
 
   const dialogContent = document.createElement('div')
-  
   const dialogHeader = document.createElement('div')
   dialogHeader.classList.add('flex', 'justify-between', 'content-start', 'mb-2', 'pb-2', 'px-4')
-
-
   const dialogTitle = document.createElement('h3')
+  dialogTitle.textContent = state.ui.activePost.title
   dialogTitle.classList.add('font-bold', 'text-lg', 'mb-2')
   const xButton = document.createElement('button')
-  xButton.classList.add(  'text-gray-500', 'cursor-pointer', 'font-semibold', 'text-lg', 'mb-2')
+  xButton.classList.add('text-gray-500', 'cursor-pointer', 'font-semibold', 'text-lg', 'mb-2')
+  // добавление svg иконки закрытия модального окна
   xButton.innerHTML = closeIconRaw
+  xButton.addEventListener('click', () => dialogWindow.close())
   dialogHeader.append(dialogTitle, xButton)
+  // создание описания поста
+  const dialogDescription = document.createElement('p')
+  dialogDescription.textContent = i18nextInstance.t(($) => $.modalWindow.goal)
+  dialogDescription.classList.add('border-y', 'border-gray-700', 'py-4', 'px-4', 'mb-2')
 
-
+  // создание контейнера для кнопок
+  const buttonsContainer = document.createElement('div')
+  buttonsContainer.classList.add('flex', 'justify-end', 'gap-2', 'px-4')
+  // создание кнопок просмотра и закрытия модального окна
   const dialogCloseButton = document.createElement('button')
   dialogCloseButton.classList.add('border', 'border-gray-300', 'px-2', 'py-1', 'rounded', 'text-gray-500', 'cursor-pointer')
-  const dialogDescription = document.createElement('p')
-  dialogDescription.classList.add('border-y', 'border-gray-700', 'py-4', 'px-4', 'mb-2')
   const modalViewButton = document.createElement('button')
-   modalViewButton.classList.add('border', 'border-cyan-500', 'px-2', 'py-2', 'bg-blue-600', 'text-white', 'rounded', 'cursor-pointer')
+  modalViewButton.classList.add('border', 'border-cyan-500', 'px-2', 'py-2', 'bg-blue-600', 'text-white', 'rounded', 'cursor-pointer')
   modalViewButton.textContent = i18nextInstance.t(($) => $.ui.buttons.readMore)
+  // создание кнопки закрытия модального окна
   const closeModalButton = document.createElement('button')
   closeModalButton.classList.add('border', 'border-gray-300', 'px-4', 'py-2', 'rounded', 'bg-gray-500', 'text-white', 'cursor-pointer')
   closeModalButton.textContent = i18nextInstance.t(($) => $.ui.buttons.close)
- 
-  dialogTitle.textContent = state.ui.activePost.title
-  dialogDescription.textContent = i18nextInstance.t(($) => $.modalWindow.goal)
-  
-  // Buttons container
-  const buttonsContainer = document.createElement('div')
-  buttonsContainer.classList.add('flex', 'justify-end', 'gap-2', 'px-4')
-  buttonsContainer.append(  modalViewButton, closeModalButton)
+  buttonsContainer.append(modalViewButton, closeModalButton)
   closeModalButton.addEventListener('click', () => dialogWindow.close())
   dialogContent.append(dialogHeader, dialogDescription, buttonsContainer)
-  dialogWindow.append(  dialogContent )
+  dialogWindow.append(dialogContent)
   container.append(dialogWindow)
-   xButton.addEventListener('click', () => dialogWindow.close())
-    
   dialogWindow.showModal()
 }
-
-
 
 export { renderMessage, renderPosts, renderFeed, renderModal }
